11518acd299e4c5955896d620118ced1d4910de5
WaadSulaiman <waad.sulaiman@outlook.com>
2022-09-12 23:45:44 +0200

    Configured SignalR, added a TransferHub.cs for file transferring.

--- src/SonarWave.Application/Program.cs
+++ src/SonarWave.Application/Program.cs
@@ -1,6 +1,18 @@
+using SonarWave.Application.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR(options =>
+{
+    options.EnableDetailedErrors = true;
+    options.ClientTimeoutInterval = TimeSpan.FromMinutes(10);
+    options.MaximumReceiveMessageSize = 1000000000;
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapHub<TransferHub>("/hubs/transferhub");
 
 app.Run();
